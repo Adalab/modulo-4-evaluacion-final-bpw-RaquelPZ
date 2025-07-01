724bd3011b9786afd4c61664c2f44855b41ca3cc
raquelpz <rpz.ra.chel@gmail.com>
2025-07-01 11:24:05 +0200

    connection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-//Server and connection
+//Imports
 const express = require('express');
 const cors = require('cors');
+const mysql = require("mysql2/promise");
+require("dotenv").config();
 
 //Server is put into a variable
 const server = express();
@@ -14,6 +16,20 @@ const port = 4000;
 server.listen(port, () => {
   console.log(`Super-servidor iniciado <http://localhost:${port}>`);
 });
+
+//Connection function
+async function getConnection() {
+  const connData = {
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+  };
+  const conn = await mysql.createConnection(connData);
+  await conn.connect();
+  return conn;
+}
 
 //ENDPOINTS API
 
