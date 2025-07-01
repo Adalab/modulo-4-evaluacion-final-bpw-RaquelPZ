8d24c3f53bce9c03894e72cea444f94b6dc6b81d
raquelpz <rpz.ra.chel@gmail.com>
2025-07-01 11:11:40 +0200

    Minimal server without errors and 404 error endpoint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 //Server and connection
-const express = requiere('express');
-const cors = requiere('cors');
+const express = require('express');
+const cors = require('cors');
+
+//Server is put into a variable
+const server = express();
 
 //Server configuration for correct operation as an API
 server.use(cors());
@@ -10,4 +13,11 @@ server.use(express.json({ limit: "50mb" })); //acepta este formato y este peso
 const port = 4000;
 server.listen(port, () => {
   console.log(`Super-servidor iniciado <http://localhost:${port}>`);
+});
+
+//ENDPOINTS API
+
+//End: Error 404
+server.use(/.*/, (req, res) => {
+  res.status(404).send("ERROR 404: Esta ruta se la inventó usted.");
 });
